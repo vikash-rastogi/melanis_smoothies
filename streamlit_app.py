0bebb3e08419dec58b13e8948e6bfa63e329bfea
vikash-rastogi <vikash.rastogi169@gmail.com>
2026-03-25 15:25:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages.
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark import Session
 from snowflake.snowpark.functions import col
 import requests  
 import pandas as pd
@@ -11,7 +12,8 @@ st.title(f"Place your order for healthy smoothie :cup_with_straw:")
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("Your name on Smoothie woule be:", name_on_order)
 
-session = get_active_session()
+# session = get_active_session()
+session = Session.builder.configs(CONNECTION_PARAMETERS).create()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
